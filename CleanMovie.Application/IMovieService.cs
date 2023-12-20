48b3063e77efca500098dec03a5e85d2bbe66e8e
Xavi <Xavi@DESKTOP-FQQGNEK>
2023-12-19 21:47:28 -0600

    Add project files.

---
+++ CleanMovie.Application/IMovieService.cs
@@ -0,0 +1,15 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public interface IMovieService
+    {
+        // Es un caso de uso
+        List<Movie> GetAllMovies();
+    }
+}
---
+++ CleanMovie.Infraestructure/MovieRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Infraestructure
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie{Id = 1, Name="Interestelar", Cost = 1},
+            new Movie{Id = 2, Name="Ciudad de Dios", Cost = 3}
+        };
+
+
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+    }
+}
